2ca194708198c7f8186b92f674aafc963b3eeca3
wanax1 <54093740+wanax1@users.noreply.github.com>
2019-08-18 21:38:49 +0800

    Update app.py
    flask web frame

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
+import flask
+from flask import *
+app=Flask(__name__)
+#flask.request为请求方式
+@app.route('/',methods=['GET',"POST"]) #mthods定义了请求的方式
+def imdex():
+    if request.method=='POST': #判断请求
+        return 'Post qingqiu !'
+    else:
+        return 'Get qinqiu !'
 
-print('Hello World! name with app.py')
+if __name__ == '__main__':
+    app.run()
